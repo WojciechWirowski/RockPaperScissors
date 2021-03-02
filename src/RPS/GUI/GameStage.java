f9864699e76aa365da70f6332972965b2064269e
Ivanio96 <71640528+Ivanio96@users.noreply.github.com>
2021-03-02 21:44:47 +0100

    GUI version 2 Menu and Game

--- src/RPS/GUI/GameStage.java
+++ src/RPS/GUI/GameStage.java
@@ -1,17 +1,26 @@
 package RPS.GUI;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
-import javax.swing.text.html.ImageView;
-import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class GameStage implements Initializable {
+
+    @FXML
+    private Button btnMenu;
 
     @FXML
     private Button btnDifficulty;
@@ -29,7 +38,7 @@ public class GameStage implements Initializable {
     private Button btnScissors;
 
     @FXML
-    private ImageView imgEnemy;
+    private ImageView imageView;
 
     @FXML
     private Label lblPlayerName;
@@ -43,12 +52,6 @@ public class GameStage implements Initializable {
     @FXML
     private Button btnSetName;
 
-    @FXML
-    void changeName(ActionEvent event) {
-
-
-    }
-
     public GameStage(){
 
 
@@ -60,6 +63,18 @@ public class GameStage implements Initializable {
 
     }
 
+
+    public void backToMenu(ActionEvent actionEvent) throws IOException {
+        Stage currentStage = (Stage) btnMenu.getScene().getWindow();
+        currentStage.close();
+
+
+        Stage primaryStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/RPS/GUI/menuStage.fxml"));
+        primaryStage.setTitle("Rock Paper Scissors Menu");
+        primaryStage.setScene(new Scene(root, 1000, 600));
+        primaryStage.show();
+    }
 
 
 }
--- src/RPS/GUI/MenuStage.java
+++ src/RPS/GUI/MenuStage.java
@@ -1,45 +1,57 @@
 package RPS.GUI;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-import org.jetbrains.annotations.NotNull;
 
+import java.awt.event.ActionEvent;
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class FirstStage implements Initializable{
-    public Label lbl1;
-    public Label lbl2;
+public class MenuStage implements Initializable{
 
-    public FirstStage(){
 
-            }
+
+
+    @FXML
+    private Button btnStartGame;
+    @FXML
+    private Label lblChooseVillain;
+
+
+    public MenuStage(){
+
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        lbl1.setText("Welcome to our game!");
-        lbl2.setText("Press anywhere to open main menu...");
+
+        lblChooseVillain.setText("CHOOSE A VILLAIN");
+
+
     }
 
 
-    public void enterGame(MouseEvent mouseEvent) throws IOException {
 
-        Stage currentStage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+    public void startGame(javafx.event.ActionEvent actionEvent) throws IOException {
+        Stage currentStage = (Stage) btnStartGame.getScene().getWindow();
         currentStage.close();
 
 
         Stage primaryStage = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("/RPS/GUI/menuStage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/RPS/GUI/gameStage.fxml"));
         primaryStage.setTitle("Rock Paper Scissors Menu");
         primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
-    }
 
+    }
 }
